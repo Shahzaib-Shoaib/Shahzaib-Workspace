6a40e17dca7130c20bf429aca1bea2507dbc05f2
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2021-09-06 18:20:40 +0500

    .

--- Project-Shoe-Shop/products/product.js
+++ Project-Shoe-Shop/products/product.js
@@ -1,5 +1,21 @@
-const addBtn = document.getElementById('buy');
-addBtn.addEventListener('click', () => {
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+const button = document.querySelector('input');
 
-    console.log('vdsgjjd');
-})
+
+
+// 2. Listen for click on open button
+open.addEventListener('click', () => modal.classList.add('show-modal') );
+
+// 3. Listen for click on close button
+close.addEventListener('click', () => modal.classList.remove('show-modal') );
+shop.addEventListener('click', () => modal.classList.remove('show-modal') );
+
+// 4. Listen for click outside of modal
+window.addEventListener('click', e => 
+    e.target === modal ? modal.classList.remove('show-modal') : false
+)
+
+
+
