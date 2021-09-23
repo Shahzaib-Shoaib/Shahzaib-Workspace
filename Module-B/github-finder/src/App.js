994917d62205b44b15d543dca7fa46429b4da04c
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2021-09-23 18:19:56 +0500

    .

---
+++ Module-B/github-finder/src/App.js
@@ -0,0 +1,14 @@
+import React , { Component , Fragment} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      <h1>Hello world!</h1>
+      </>
+    );
+  }
+}
+
+export default App;
