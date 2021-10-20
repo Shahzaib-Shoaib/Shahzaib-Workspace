9b1ea85e232cc1d010538ccfb899343238bf24b1
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2021-10-20 14:36:53 +0500

    .

--- Module-B/github-finder/src/App.js
+++ Module-B/github-finder/src/App.js
@@ -1,31 +1,36 @@
-import React , { Component} from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
-import Users from './components/users/Users';
-import axios from 'axios';
 
-class App extends Component {
-  state = {
-    users: [],
-    loading: false
-  }
-
-  async componentDidMount( {
-    this.setState( { loading: true } );
-    const res = await axios.get('https://api.github.com/users')
-    this.setState( { users: res.data, loading: false } )
-  })
-
-  render() {
-    return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-      <Users />
-      </div>
-    </div>
-    );
-  }
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );  
 }
 
 export default App;
--- Module-B/github-finder/src/components/users/UserItem.js
+++ Module-B/github-finder/src/components/users/UserItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const UserItem = {props} => {
-        const {login, avatar_url , html_url}= cprops.user;
-        return (
-            <div className="card text-center">
-                <img src={avatar_url} alt="Profile" className="round-img" style={{width: '60px'}} />
-                <h3>{login}</h3>
-                <a href={html_url} className="btn btn-dark btn-sm my-1"> Profile</a>
-            </div>
-        )
-    }
+const UserItem = ({ user: { login, avatar_url, html_url }}) => {
+    return (
+        <div className="card text-center">
+            <img src={avatar_url} alt='Profile' className="round-img" style={{ width: '60px' }} />
+            <h3>{login}</h3>
+            <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">Profile</Link>
+        </div>
+    )
+}
 
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+}
 
 export default UserItem
