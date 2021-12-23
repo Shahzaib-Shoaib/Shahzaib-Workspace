d65da86568b4054d9c8ba0ef23c8e2333bb42d9d
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2021-12-23 22:04:16 +0500

    Update index.js

--- Module-B/shopify-next.js-tailwind-project/pages/index.js
+++ Module-B/shopify-next.js-tailwind-project/pages/index.js
@@ -43,6 +43,6 @@ export async function getStaticProps() {
   const products = await getProductsInCollection();
 
   return {
-    props: { products }, // will be passed to the page component as props.
+    props: { products }, // will be passed to the page component as props..
   };
 }
