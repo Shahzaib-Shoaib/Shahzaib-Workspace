36389b566ff698e5e898b5e63b4686d69141d836
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2022-01-07 00:36:23 +0500

    Update Hero.js

--- Module-B/shoe-store-shopify-next.js-tailwind-project/components/Hero.js
+++ Module-B/shoe-store-shopify-next.js-tailwind-project/components/Hero.js
@@ -63,7 +63,7 @@ export default function Hero() {
       <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
         <img
           className='h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full'
-          src='../assets/'
+          src='https://raw.githubusercontent.com/Shahzaib-Shoaib/Axiom-FSD-Module-1/main/Module-B/shoe-store-shopify-next.js-tailwind-project/assets/Sample%2021.png'
           alt=''
         />
       </div>
