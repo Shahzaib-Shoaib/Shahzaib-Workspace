94f7ed5cd9df4a8d2608483d4845a5057f3a6cc3
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2022-11-14 19:13:09 +0500

    .

--- try/try.js
+++ try/try.js
@@ -1,5 +1,5 @@
 function getWebsites() {
-  return fetch("data.json")
+  return fetch("section1.json")
     .then((response) => response.json())
     .then((data) => {
       return data;
@@ -30,28 +30,137 @@ function showWebsites(websites) {
   websitesContainer.innerHTML = websitesHTML;
 }
 
+
 // when the application is loaded
 getWebsites().then((data) => {
   showWebsites(data);
+  console.log(data);
 });
 
-// <div class="job-tile">
-// <div class="top">
-//     <img src="${job.logo}" />
-//     <span class="material-icons more_horiz">more_horiz</span>
-// </div>
-// <div class="rolename">
-//     <span>${job.roleName}</span>
-// </div>
-// <div class="description">
-//     <span>${job.requirements.content}</span>
-// </div>
-// <div class="buttons">
-//     <div class="button apply-now">
-//         Apply Now
-//     </div>
-//     <div class="button">
-//         Message
-//     </div>
-// </div>
-// </div>
+////////////////////////////////////////////////////////////////
+function getWebsites2() {
+  return fetch("section2.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+}
+
+function showWebsites2(websites) {
+  let websitesContainer = document.querySelector(".websites-container2");
+  let websitesHTML = "";
+  websites.forEach((website) => {
+    websitesHTML += `
+  			<div class="row work-row"> 
+				<div class= ${website.imgClass} >
+					<img src=${website.imgSrc} alt="">
+				</div>
+				<div class=${website.detailsClass}>
+					<div class="details details-ftm">
+						<h3>${website.name}</h3>
+						<p class="platform">Web</p>
+						<p class="icons"><i class="fi-monitor"></i><i class="fi-tablet-portrait"></i><i class="fi-mobile"></i></p>
+						<p>${website.detail}</p>
+						<a class="my-button" href=${website.linkUrl}>Visit Website</a>
+					</div>
+				</div>
+			</div>
+        `;
+  });
+  websitesContainer.innerHTML = websitesHTML;
+}
+
+
+// when the application is loaded
+getWebsites2().then((data) => {
+  showWebsites2(data);
+  console.log(data);
+});
+
+
+
+////////////////////////////////////////////////////////////
+
+function getWebsites3() {
+  return fetch("section3.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+}
+
+function showWebsites3(websites) {
+  let websitesContainer = document.querySelector(".websites-container3");
+  let websitesHTML = "";
+  websites.forEach((website) => {
+    websitesHTML += `
+  			<div class="row work-row"> 
+				<div class= ${website.imgClass} >
+					<img src=${website.imgSrc} alt="">
+				</div>
+				<div class=${website.detailsClass}>
+					<div class="details details-ftm">
+						<h3>${website.name}</h3>
+						<p class="platform">Web</p>
+						<p class="icons"><i class="fi-monitor"></i><i class="fi-tablet-portrait"></i><i class="fi-mobile"></i></p>
+						<p>${website.detail}</p>
+						<a class="my-button" href=${website.linkUrl}>Visit Website</a>
+					</div>
+				</div>
+			</div>
+        `;
+  });
+  websitesContainer.innerHTML = websitesHTML;
+}
+
+
+// when the application is loaded
+getWebsites3().then((data) => {
+  showWebsites3(data);
+  console.log(data);
+});
+
+
+
+
+////////////////////////////////////////////////////////////
+
+function getWebsites4() {
+  return fetch("section4.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+}
+
+function showWebsites4(websites) {
+  let websitesContainer = document.querySelector(".websites-container4");
+  let websitesHTML = "";
+  websites.forEach((website) => {
+    websitesHTML += `
+  			<div class="row work-row"> 
+				<div class= ${website.imgClass} >
+					<img src=${website.imgSrc} alt="">
+				</div>
+				<div class=${website.detailsClass}>
+					<div class="details details-ftm">
+						<h3>${website.name}</h3>
+						<p class="platform">Web</p>
+						<p class="icons"><i class="fi-monitor"></i><i class="fi-tablet-portrait"></i><i class="fi-mobile"></i></p>
+						<p>${website.detail}</p>
+						<a class="my-button" href=${website.linkUrl}>Visit Website</a>
+					</div>
+				</div>
+			</div>
+        `;
+  });
+  websitesContainer.innerHTML = websitesHTML;
+}
+
+
+// when the application is loaded
+getWebsites4().then((data) => {
+  showWebsites4(data);
+  console.log(data);
+});
+
