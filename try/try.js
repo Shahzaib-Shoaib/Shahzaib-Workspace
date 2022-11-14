37bfcae3f74a1b6e100e77c30c8a6a7e00c90750
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2022-11-14 18:01:59 +0500

    .

--- try/try.js
+++ try/try.js
@@ -1,43 +1,57 @@
+function getWebsites() {
+  return fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+}
+
 function showWebsites(websites) {
-  let websitesContainer = document.querySelector(".website-container");
+  let websitesContainer = document.querySelector(".websites-container");
   let websitesHTML = "";
   websites.forEach((website) => {
     websitesHTML += `
-        <div class="row work-row"> <!-- odd -->
-        <div class="small-12 medium-6 medium-push-6 columns">
-            <img src="/bootcamp-developer-portfolio/developer-challenge-portfolio/assets/images/Captures/HTML Form.png" alt="">
-        </div>
-        <div class="small-12 medium-5 medium-pull-7 columns">
-            <div class="details details-ftm">
-                <h3>HTML Form</h3>
-                <p class="platform">Web</p>
-                <p class="icons"><i class="fi-monitor"></i><i class="fi-tablet-portrait"></i><i class="fi-mobile"></i></p>
-                <p>Simple HTML,CSS Signup form with JS Validation. A project from Axiom FSD Module-A.</p>
-                <a class="my-button" href="./Axiom-Module-A/Project-1/index.html">Visit Website</a>
-            </div>
-        </div>
-    </div>
+  			<div class="row work-row"> 
+				<div class= ${website.imgClass} >
+					<img src=${website.imgSrc} alt="">
+				</div>
+				<div class=${website.detailsClass}>
+					<div class="details details-ftm">
+						<h3>${website.name}</h3>
+						<p class="platform">Web</p>
+						<p class="icons"><i class="fi-monitor"></i><i class="fi-tablet-portrait"></i><i class="fi-mobile"></i></p>
+						<p>${website.detail}</p>
+						<a class="my-button" href=${website.linkUrl}>Visit Website</a>
+					</div>
+				</div>
+			</div>
         `;
   });
   websitesContainer.innerHTML = websitesHTML;
 }
 
-// <div class="row work-row"> <!-- odd -->
-// <div class="small-12 medium-6 medium-push-6 columns">
-//     <img src="/bootcamp-developer-portfolio/developer-challenge-portfolio/assets/images/Captures/HTML Form.png" alt="">
-// </div>
-// <div class="small-12 medium-5 medium-pull-7 columns">
-//     <div class="details details-ftm">
-//         <h3>HTML Form</h3>
-//         <p class="platform">Web</p>
-//         <p class="icons"><i class="fi-monitor"></i><i class="fi-tablet-portrait"></i><i class="fi-mobile"></i></p>
-//         <p>Simple HTML,CSS Signup form with JS Validation. A project from Axiom FSD Module-A.</p>
-//         <a class="my-button" href="./Axiom-Module-A/Project-1/index.html">Visit Website</a>
-//     </div>
-// </div>
-// </div>
-
 // when the application is loaded
 getWebsites().then((data) => {
   showWebsites(data);
 });
+
+// <div class="job-tile">
+// <div class="top">
+//     <img src="${job.logo}" />
+//     <span class="material-icons more_horiz">more_horiz</span>
+// </div>
+// <div class="rolename">
+//     <span>${job.roleName}</span>
+// </div>
+// <div class="description">
+//     <span>${job.requirements.content}</span>
+// </div>
+// <div class="buttons">
+//     <div class="button apply-now">
+//         Apply Now
+//     </div>
+//     <div class="button">
+//         Message
+//     </div>
+// </div>
+// </div>
